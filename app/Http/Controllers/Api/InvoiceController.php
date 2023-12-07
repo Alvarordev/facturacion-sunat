eba0fbe88a776e6b0c0e474f86cff72c11823e5d
alvaro <alvarord519@gmail.com>
2023-12-06 19:37:07 -0500

    Cambie el servicio sunat para que la creacion de facturas sea dinamicas y agregue calculos de los campos que son obviables

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -4,26 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\SunatService;
-use DateTime;
 use Illuminate\Http\Request;
-use Greenter\Model\Client\Client;
-use Greenter\Model\Company\Company;
-use Greenter\Model\Company\Address;
-use Greenter\Model\Sale\FormaPagos\FormaPagoContado;
-use Greenter\Model\Sale\Invoice;
-use Greenter\Model\Sale\SaleDetail;
-use Greenter\Model\Sale\Legend;
 use Greenter\Report\XmlUtils;
+use Luecano\NumeroALetras\NumeroALetras;
 
 class InvoiceController extends Controller
 {
     public function send(Request $request)
     {
        
+        $data = $request->all();
+
+        $this->setTotales($data);
+        $this->setLegends($data);
+
         $sunat = new SunatService();
 
         $see = $sunat -> getsee();
-        $invoice = $sunat->getInvoice();
+        $invoice = $sunat->getInvoice($data);
         $result = $see->send($invoice);
 
         $response['xml'] = $see->getFactory()->getLastXml();
@@ -31,5 +29,38 @@ class InvoiceController extends Controller
         $response['sunatResponse'] = $sunat->sunatResponse($result);
 
         return response()->json($response, 200);
+    }
+
+    public function setTotales(&$data){
+        $details = collect($data['details']);
+
+        $data['mtoOperGravadas'] = $details->where('tipAfeIgv', 10)->sum('mtoValorVenta');
+        $data['mtoOperExoneradas'] = $details->where('tipAfeIgv', 20)->sum('mtoValorVenta');
+        $data['mtoOperInafectas'] = $details->where('tipAfeIgv', 30)->sum('mtoValorVenta');
+        $data['mtoOperExportacion'] = $details->where('tipAfeIgv', 40)->sum('mtoValorVenta');
+        $data['mtoOperGratuitas'] = $details->whereNotIn('tipAfeIgv', [10, 20, 30, 40])->sum('mtoValorVenta');
+
+        $data['mtoIGV'] = $details->whereIn('tipAfeIgv', [10, 20, 30, 40])->sum('igv');
+        $data['mtoIGVGratuitas'] = $details->whereNotIn('tipAfeIgv', [10, 20, 30, 40])->sum('igv');
+        $data['icbper'] = $details->sum('icbper');
+        $data['totalImpuestos'] = $data['mtoIGV'] + $data['icbper'];
+
+        $data['valorVenta'] = $details->whereIn('tipAfeIgv', [10, 20, 30, 40])->sum('mtoValorVenta');
+        $data['subTotal'] = $data['valorVenta'] + $data['totalImpuestos'];
+
+        $data['mtoImpVenta'] = floor($data['subTotal'] * 10) / 10;
+
+        $data['redondeo'] = $data['mtoImpVenta'] - $data['subTotal'];
+    }
+
+    public function setLegends(&$data){
+        $formatter = new NumeroALetras();
+
+        $data['legends'] = [
+            [
+                'code' => '1000',
+                'value' => $formatter->toInvoice($data['mtoImpVenta'], 2, 'SOLES')
+            ]
+        ];
     }
 }
